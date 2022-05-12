865d366da849e5252d548c37f78aba91f880f9f8
sun <1282854848@qq.com>
2022-05-12 10:19:23 +0800

    push commit

--- src/com/mygit/test/test1.java
+++ src/com/mygit/test/test1.java
@@ -4,5 +4,8 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("git1");
         System.out.println("git4");
+        System.out.println("push test");
+
+
     }
 }
